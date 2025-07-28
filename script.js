26403578380c0607326c66b3224b28336620382b
uchixazokir <uchixaitaachi.71@gmail.com>
2025-07-28 14:52:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+ let data = [1,2,3,4,5,6,7,8,9]
+
+ console.log(data);
+
+ let juft = []
+ let toq = []
+
+ for(i=0; i<data.length; i++){
+    if(data[i] % 2== 0){
+        juft.push(data[i])
+    }else{
+        toq.push(data[i])
+    }
+ }
+ console.log(juft);
+ console.log(toq);
+ 
+ 
+ 
